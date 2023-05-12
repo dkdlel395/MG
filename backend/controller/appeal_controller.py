a87b2014d4f953fd4b3098e825b5ce6714cb2705
dkdlel395 <74848389+dkdlel395@users.noreply.github.com>
2023-05-12 10:38:51 +0900

    feat: mgti result 기능 수정

--- backend/controller/appeal_controller.py
+++ backend/controller/appeal_controller.py
@@ -1,5 +1,4 @@
-from flask import render_template, request, Flask, jsonify
-import requests
+from flask import render_template, request, Flask, jsonify, session
 from controller import bp_appeal as appeal
 from controller import db_controller
 import time
@@ -9,70 +8,68 @@ db = db_controller
 app = db.init_database()
 mysql = db.init(app)
 
-appeal_app = Flask(__name__)
 
 messages = []  # 채팅 기록 저장
 
+
 @appeal.route('/', methods=['GET', 'POST'])
 def home():
-    from chat_gpt import gpt_plugin
-
-    text = gpt_plugin.gpt('안녕')
-
-    return text    
+    return 'appeal'
 
 
 # 프론트에서 쓰여진 채팅 받아오는 함수
 @appeal.route("/upload_text", methods=["POST"])
 def upload_text():
     user_name = "user_name"
-    data = request.data.decode('utf-8')
+    message_content = request.form.get('msg')
     current_time = time.time()
     message = {
         'message_type': 'chat',
         'user_name': user_name,
-        'message_content': data,
+        'message_content': message_content,
         'time': current_time
     }
     messages.append(message)
-    print(messages)
-    return jsonify(message)  # message 안에는 (messsagetype(사용자인지,봇인지 판단), 사용자 이름, 사용자 채팅 내용, 시간) 으로 이루어져있다.
+    # message 안에는 (messsagetype(사용자인지,봇인지 판단), 사용자 이름, 사용자 채팅 내용, 시간) 으로 이루어져있다.
+    return jsonify(message)
 
 # 챗봇 응답 처리
+
+
 @appeal.route("/answer", methods=["POST"])
 def answer():
-    data = request.data.decode('utf-8')
-
-    from chat_gpt import gpt_plugin
-    text = gpt_plugin.gpt(data)
-
+    answer = "gpt답변"
     current_time = time.time()
     message = {
         'message_type': 'bot',
         'dog_name': '뽀또',
-        'message_content': text,
+        'message_content': answer,
         'time': current_time
     }
     messages.append(message)
-    return jsonify(message) # message 안에는 (messsagetype(사용자인지,봇인지 판단), 강아지 이름, 강아지 채팅 내용, 시간) 으로 이루어져있다.
+    # message 안에는 (messsagetype(사용자인지,봇인지 판단), 강아지 이름, 강아지 채팅 내용, 시간) 으로 이루어져있다.
+    return jsonify(message)
 
 
 # 채팅 페이지
 @appeal.route("/chat", methods=["GET", "POST"])
 def chatting():
     user_input = request.form.get("msg")
-    
     response = answer()
-    chat_get_dog_info = db.chat_get_dog_info(app,mysql,1111111111) 
+    chat_get_dog_info = db.chat_get_dog_info(app, mysql, 1111111111)
     chat_user_id_name_pic = db.chat_user_id_name_pic()
-    return render_template("chat.html", user_input=user_input, response=response, messages=messages,chat_get_dog_info=chat_get_dog_info,chat_user_id_name_pic=chat_user_id_name_pic)
+    return render_template("chat.html", user_input=user_input, response=response, messages=messages, chat_get_dog_info=chat_get_dog_info, chat_user_id_name_pic=chat_user_id_name_pic)
 
 # MGTI 시작 페이지
+
+
 @appeal.route('/mgti_start', methods=['GET', 'POST'])
 def mgti_start():
     return render_template("mgti_start.html")
 
 # MGTI 진행 페이지
+
+
 @appeal.route('/mgti_ing', methods=['GET', 'POST'])
 def mgti_ing():
     return render_template("mgti_ing.html")
@@ -84,37 +81,49 @@ def mgti_res():
     return render_template("mgti_res.html")
 
 # MGTI 고른거 전송 라우트
+
+
 @appeal.route('/res2', methods=['get', 'post'])
 def res2():
     if request.method == 'POST':
         I, E, S, N, T, J, F, P = 0, 0, 0, 0, 0, 0, 0, 0
         data = request.get_json()
     for key, value in data.items():
-        if key == '0' or key == '1' or key == '3':
+        if key == '0' or key == '1' or key == '2':
             if value == '0':
                 I += 1
             elif value == '1':
                 E += 1
-        elif key == '2' or key == '6' or key == '7':
+        elif key == '3':
             if value == '0':
-                N += 2
+                I += 2 
             elif value == '1':
-                S += 2
-        elif key == '4' or key == '5':
+                E += 2
+        elif key == '4' or key == '6' or key == '7':
             if value == '0':
                 N += 1
             elif value == '1':
                 S += 1
-        elif key == '8' or key == '9' or key == '10' or key == '11':
+        elif key == '5':
+            if value == '0':
+                N += 2
+            elif value == '1':
+                S += 2
+        elif key == '9' or key == '10' or key == '11':
             if value == '0':
                 T += 1
             elif value == '1':
                 F += 1
+        elif key == '8':
+            if value == '0':
+                T += 2
+            elif value == '1':
+                F += 2
         elif key == '12' or key == '13' or key == '14':
             if value == '0':
                 J += 1
             elif value == '1':
-                P += 1
+                P += 1 
 
     if I > E:
         A = "I"
